412ceaa278df384130720a9061f004cd44a1d897
SrSherman333 <misatandazobrito12@gmail.com>
2026-03-13 22:47:31 -0500

    docs (models.py): Documenting the code (docstrings and comments)

--- src/core/models.py
+++ src/core/models.py
@@ -1,12 +1,28 @@
+"""
+Table format.
+
+Specific template or format for the data in the Categories and Transactions tables
+
+- dataclasses: To create simple classes that automatically locate the data
+"""
+
 from dataclasses import dataclass
 
 @dataclass
 class Category:
+    """
+    Class with the format for the data in the Categories table
+    """
+    
     category_name: str = ""
     category_type: str = ""
     
 @dataclass
 class Transaction:
+    """
+    Class with the format for the data in the Transactions tables
+    """
+    
     date: str = ""
     concept: str = ""
     amount: float = 0.0
